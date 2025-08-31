e8cc8f200f9f927ab623c5b8c6c2fceaed8b2e4c
nnsncl <nuniroland@gmail.com>
2025-08-31 11:43:27 +0200

    Enhance landing page's UI

--- src/app/(home)/_components/CommandTrigger.tsx
+++ src/app/(home)/_components/CommandTrigger.tsx
@@ -3,8 +3,21 @@
 import React from "react";
 import styled from "styled-components";
 
-import { Badge, Button } from "@usefui/components";
+import { Button } from "@usefui/components";
 import { Icon, PixelIcon } from "@usefui/icons";
+
+const TerminalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: var(--breakpoint-tablet);
+  margin: 0 auto;
+  border: var(--measurement-small-30) solid var(--font-color-alpha-10);
+  border-radius: var(--measurement-medium-30);
+
+  background: var(--contrast-color);
+`;
 
 function CommandTrigger() {
   const [copied, setCopied] = React.useState(false);
@@ -21,11 +34,15 @@ function CommandTrigger() {
   };
 
   return (
-    <Badge variant="border">
+    <TerminalContainer>
       <div className="p-medium-60 flex align-center justify-between g-large-10">
-        <code className="fs-medium-10 opacity-default-60">
-          npx @usefui/svgjsx generate
-        </code>
+        <span className="flex align-center g-medium-30">
+          <kbd className="fs-medium-10 opacity-default-10">$</kbd>
+          <kbd className="fs-medium-10 opacity-default-60">
+            npx @usefui/svgjsx generate
+          </kbd>
+        </span>
+
         <Button
           variant="ghost"
           sizing="small"
@@ -34,7 +51,7 @@ function CommandTrigger() {
           <Icon>{copied ? <PixelIcon.Check /> : <PixelIcon.Duplicate />}</Icon>
         </Button>
       </div>
-    </Badge>
+    </TerminalContainer>
   );
 }
 
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,21 +1,16 @@
 "use client";
 
 import React from "react";
-
 import LandingHero from "./_components/LandingHero";
-import AnimatedHero from "./_components/AnimatedHero";
 
 import { WebNavigation } from "@/components";
 
 function LandingPage() {
   return (
-    <div className="h-100 w-100" style={{ position: "relative" }}>
+    <React.Fragment>
       <WebNavigation />
-
       <LandingHero />
-      <AnimatedHero chars="svgjsx" />
-      {/* <Footer /> */}
-    </div>
+    </React.Fragment>
   );
 }
 
