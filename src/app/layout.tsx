41c3eae670bf0ca2589a98bb0a36a5664b9516b7
nnsncl <nuniroland@gmail.com>
2025-11-02 17:18:39 +0100

    Add meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,94 @@
+import type { Metadata, Viewport } from "next";
+
 import StyledComponentsRegistry from "@/lib/styles-registry";
 
 import { ClientProvider } from "@/providers";
 import { Toaster } from "sonner";
+
+const siteUrl = "https://svgjsx.dev";
+const siteName = "SVGJSX";
+const appTitle = "SVGJSX | Foundation UI";
+const appDescription =
+  "Powerful CLI built for modern product teams to convert and bundle SVG files into component libraries";
+
+export const metadata: Metadata = {
+  title: {
+    default: appTitle,
+    template: "%s | SVGJSX",
+  },
+
+  description: appDescription,
+  applicationName: siteName,
+
+  keywords: [
+    "Foundation UI",
+    "CLI",
+    "Headless Design System",
+    "Icons generator",
+    "Icons library",
+    "UI components",
+    "React icon library",
+    "Nextjs icon library",
+  ],
+
+  authors: [{ name: "Nicolas Nunes", url: "https://github.com/nnsncl" }],
+  creator: "Foundation UI",
+  publisher: "Foundation UI",
+  generator: "Next.js",
+
+  metadataBase: new URL(siteUrl),
+  alternates: {
+    canonical: siteUrl,
+  },
+
+  category: "technology",
+  classification: "Software",
+  referrer: "strict-origin-when-cross-origin",
+
+  // Open Graph
+  openGraph: {
+    title: appTitle,
+    description: appDescription,
+    url: siteUrl,
+    siteName,
+    images: [],
+    locale: "en_US",
+    type: "website",
+  },
+
+  // Twitter
+  twitter: {
+    card: "summary_large_image",
+    title: appTitle,
+    description: appDescription,
+    images: [],
+  },
+
+  // Robots
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+
+  // Icons
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+  },
+};
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  viewportFit: "cover",
+};
 
 export default function RootLayout({
   children,
